1028691b131da40b8110474d8ebd0c8b1f80fbe4
abhishekshah <56739982+abhishekshah0036@users.noreply.github.com>
2021-05-08 14:57:31 +0530

    week 2

--- Web_development/food-chain/src/components/MainComponent.js
+++ Web_development/food-chain/src/components/MainComponent.js
@@ -1,35 +1,62 @@
 import React, { Component } from 'react';
-import Menu from './MenuComponents';
 import Home from './HomeComponent';
-import Header from './HeaderComponent.js';
+import Menu from './MenuComponents';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { Contact } from './ContactComponent';
 import { DISHES } from '../shared/dishes';
-import {Route,Redirect,Switch} from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotion';
+import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES, 
+            comments: COMMENTS, 
+            leaders: LEADERS, 
+            promotions: PROMOTIONS
+        };
+    }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path ='/Home' component = {() => <Home />} />
-          <Route path ='/Menu' component = {() => <Menu dishes={this.state.dishes} />} />
-          <Route path ='/contactus' component ={() => <Contact />} />
-          <Redirect path='/Home' />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        const HomePage = () => {
+            return(
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}    
+                />
+            );
+          }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                            comments={this.state.comments.filter((comment) => comment.id === parseInt(match.params.dishId,10))[0]}
+                />
+            );
+        }  
+
+        return (
+            <div>
+                <Header />
+                    <Switch> 
+                        <Route path='/home' component={HomePage} />
+                        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                        <Route path="/menu/:dishID" component={DishWithId} />
+                        <Route exact path="/contactus" component={Contact} />
+                        <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+                        <Redirect to="/home" />  
+                    </Switch>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Main;
--- Web_development/food-chain/src/components/MenuComponents.js
+++ Web_development/food-chain/src/components/MenuComponents.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {Card,CardImg,CardImgOverlay,CardTitle} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import {Breadcrumb, BreadcrumbItem, Card,CardImg,CardImgOverlay,CardTitle} from 'reactstrap';
  
     function RenderMenu ({dish}) {
         return(
-            <Card key={dish.id} >
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+            <Card>
+                <Link to={`/Menu/${dish.id}`}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>
         );
     }
     
     const Menu = (props) => {
-            const menu = props.dishes.map((dish) => {
+            
+        const menu = props.dishes.map((dish) => {
                 return (
                     <div  className="col-12 col-md-5 m-1  bg-white rounded">
                         <RenderMenu dish = {dish} />
@@ -21,13 +25,22 @@ import {Card,CardImg,CardImgOverlay,CardTitle} from 'reactstrap';
                 );
             });
         
-                return (
-                    <div className="container">
-                        <div className="row">
-                            {(props.dishes != null)? menu :<div></div>}
-                        </div>
+        return (
+            <div className="container">
+                <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/Home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>Menu</h3>
                     </div>
-                );
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
     }
 
 export default Menu;
